34a861ac13719a0b0dfb56b8bea4bcb90ad8f198
mmilanovic <marko.milanovic@barrage.net>
2022-12-11 22:51:26 +0100

    tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: {
+          50: "#DEDBFF",
+          100: "#B8B3FF",
+          200: "#756BFF",
+          300: "#2E1FFF",
+          400: "#0E00D1",
+          500: "#090088",
+          600: "#07006B",
+          700: "#050052",
+          800: "#040038",
+          900: "#020019",
+        },
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
   },
   plugins: [],
 };
